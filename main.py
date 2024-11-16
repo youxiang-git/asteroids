211b96ec55defdc583e1fcd27b5795359221046f
youxiang-git <chaiyouxiang@gmail.com>
2024-11-16 18:04:11 +0800

    create player sprite

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +16,10 @@ def main():
                 return
         
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
